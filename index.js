f03c09b6922a12db1cd46e0c0435e2f4dfcd06db
juan rebolledo <juanjoserebolledo18@gmail.com>
2022-07-13 23:17:36 -0400

    ...

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import { customAlphabet } from 'nanoid';
+const nanoid = customAlphabet('1234567890', 3);
 const app = express();
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const shortUrlSchema = new mongoose.Schema({
+  original_url: {
+    type: String,
+    required: true
+  },
+  short_url: {
+    type: String,
+    required: true
+  }
+});
+const ShortUrl = mongoose.model('ShortUrl', shortUrlSchema);
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(bodyParser.json());
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -15,10 +41,33 @@ app.get('/', function(req, res) {
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', async (req, res) => {
+  const urlData = req.body.url;
+  const shortData = nanoid();
+
+  if (urlData.startsWith('http')) {
+    const record = new ShortUrl({
+      original_url: urlData,
+      short_url: shortData
+    });
+    await record.save();
+    res.json({original_url: urlData, short_url: shortData});
+  } else {
+    res.json({error: 'invalid url'});
+  }
 });
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+app.get('/api/shorturl/:shortId', (req, res) => {
+  ShortUrl.findOne({short_url: req.params.shortId}, (err, url) => {
+    if (err) console.log(err);
+    res.redirect(url.original_url);
+  });
 });
+
+mongoose.connection.on('open', () => {
+  app.listen(port, function() {
+    console.log(`Listening on port ${port}`);
+  });
+});
+
+
